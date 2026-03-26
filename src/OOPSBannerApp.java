7ee39e3395051fdfa9052095f4e5bb299df9ed62
kartikpathak414 <kartikpathak414@gmail.com>
2026-03-26 20:01:15 +0530

    UC4: Use array and loop for banner

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -2,14 +2,20 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join("", " *****   ", " *****   ", " *****   ", " ***** "));
-        System.out.println(String.join("", "*     * ", "*     * ", "*     * ", "*     *"));
-        System.out.println(String.join("", "*     * ", "*     * ", "*     * ", "*     *"));
-        System.out.println(String.join("", "*     * ", " *****   ", " *****   ", " ***** "));
-        System.out.println(String.join("", "*     * ", "*       ", "    *    ", "    * "));
-        System.out.println(String.join("", "*     * ", "*       ", "    *    ", "    * "));
-        System.out.println(String.join("", " *****   ", "*       ", "    *    ", " ***** "));
+        String[] banner = {
+                String.join("", " *****   ", " *****   ", " *****   ", " ***** "),
+                String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
+                String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
+                String.join("", "*     * ", " *****   ", " *****   ", " ***** "),
+                String.join("", "*     * ", "*       ", "    *    ", "    * "),
+                String.join("", "*     * ", "*       ", "    *    ", "    * "),
+                String.join("", " *****   ", "*       ", "    *    ", " ***** ")
+        };
+
+        // Loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
 
     }
-
 }
