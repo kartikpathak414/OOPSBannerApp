b5c53a5e76290a0f7476275c3e4de9e083086aaa
kartikpathak414 <kartikpathak414@gmail.com>
2026-03-26 13:58:29 +0530

    Initial commit: UC1 Print OOPS

---
+++ src/OOPSBannerApp.java
@@ -0,0 +1,11 @@
+/**
+ * OOPSBannerApp - UC1
+ * Prints "OOPS" to the console
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+
+}
